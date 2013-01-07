72cb783ff25c93010b6aef262d2fd5ee70427d14
Paweł Wiergowski <goyello@pwiergowski-pc.goyello.net>
2013-01-07 18:07:41 +0100

    Initial Commit

---
+++ QuartzCoreStudy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QuartzCoreStudy
+//
+//  Created by Paweł Wiergowski on 1/7/13.
+//  Copyright (c) 2013 Paweł Wiergowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
